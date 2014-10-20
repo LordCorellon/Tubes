8f1d6e43e177d1f31936eced3c70ae510ca525fb
LordCorellon <tiamatgm@hotmail.com>
2014-10-19 22:37:40 -0400

    Add blocking for non-requesting tubes
    New variable mIsOpen to control blocking items entering a inactive requesting tube set to the same item

--- src/main/java/schmoller/tubes/types/RequestingTube.java
+++ src/main/java/schmoller/tubes/types/RequestingTube.java
@@ -10,28 +10,36 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.MovingObjectPosition;
 import net.minecraftforge.common.util.Constants;
+import net.minecraftforge.common.util.ForgeDirection;
+import schmoller.tubes.AnyFilter;
 import schmoller.tubes.ItemFilter;
 import schmoller.tubes.ModTubes;
 import schmoller.tubes.PullMode;
 import schmoller.tubes.api.FilterRegistry;
+import schmoller.tubes.api.InteractionHandler;
 import schmoller.tubes.api.OverflowBuffer;
 import schmoller.tubes.api.Payload;
 import schmoller.tubes.api.Position;
 import schmoller.tubes.api.SizeMode;
 import schmoller.tubes.api.TubeItem;
-import schmoller.tubes.api.TubesAPI;
+import schmoller.tubes.api.helpers.CommonHelper;
 import schmoller.tubes.api.helpers.TubeHelper;
 import schmoller.tubes.api.helpers.BaseRouter.PathLocation;
 import schmoller.tubes.api.interfaces.IFilter;
-import schmoller.tubes.api.interfaces.IPropertyHolder;
+import schmoller.tubes.api.interfaces.IPayloadHandler;
+import schmoller.tubes.api.interfaces.IRouteCheckCallback;
+import schmoller.tubes.api.interfaces.ITube;
 import schmoller.tubes.api.interfaces.ITubeConnectable;
 import schmoller.tubes.api.interfaces.ITubeImportDest;
 import schmoller.tubes.api.interfaces.ITubeOverflowDestination;
-import schmoller.tubes.routing.GoalRouter;
+import schmoller.tubes.routing.ImportSourceFinder;
+import schmoller.tubes.routing.InputRouter;
+import schmoller.tubes.routing.OutputRouter;
 
-public class RequestingTube extends DirectionalTube implements ITubeImportDest, IRedstonePart, ITubeOverflowDestination, IPropertyHolder
+public class RequestingTube extends DirectionalTube implements ITubeImportDest, IRedstonePart, ITubeOverflowDestination, IRouteCheckCallback
 {
 	private IFilter[] mFilter = new IFilter[16];
 	private int mNext = 0;
@@ -41,6 +49,7 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 	private int mColor = -1;
 	
 	private int mPulses = 0;
+	private int mIsOpen = 0;
 	private boolean mIsPowered;
 	
 	public static final int CHANNEL_PULSE = 2;
@@ -48,11 +57,9 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 	public static final int CHANNEL_POWERED = 4;
 	public static final int CHANNEL_SIZEMODE = 5;
 	
-	public static final int PROP_MODE = 1;
-	public static final int PROP_SIZEMODE = 2;
-	public static final int PROP_COLOR = 3;
-	
 	public float animTime = 0;
+	
+	private IFilter mActiveFilter;
 	
 	public RequestingTube()
 	{
@@ -104,12 +111,11 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 		if(!mOverflow.isEmpty())
 		{
 			TubeItem item = mOverflow.peekNext();
-			PathLocation loc = new GoalRouter(world(), new Position(x(),y(),z()), item, getFacing() ^ 1, TubesAPI.goalOutput).route();
-			
+			PathLocation loc = new OutputRouter(world(), new Position(x(),y(),z()), item, getFacing() ^ 1).route();
 			if(loc != null)
 			{
 				mOverflow.getNext();
-				item.goal = TubesAPI.goalOutput;
+				item.state = TubeItem.NORMAL;
 				item.direction = item.lastDirection = getFacing() ^ 1;
 				item.updated = true;
 				item.setProgress(0.5f);
@@ -128,29 +134,77 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 			}
 			while(filterItem == null && mNext != start);
 			
-			TubeItem item = TubeHelper.requestImport(world(), new Position(x(), y(), z()), getFacing(), filterItem, mSizeMode, -1, null);
-			if(item != null)
+			
+			mActiveFilter = filterItem;
+			PathLocation source = new ImportSourceFinder(world(), new Position(x(), y(), z()), getFacing(), filterItem, mSizeMode).setRouteCheckCallback(this).route();
+			if(source != null)
 			{
-				--mPulses;
-				if(mPulses < 0)
-					mPulses = 0;
-				
-				if(mMode == PullMode.RedstoneSingle)
-					openChannel(CHANNEL_PULSE);
+				IPayloadHandler handler = InteractionHandler.getHandler((filterItem == null ? null : filterItem.getPayloadType()), world(), source.position);
+				if(handler != null)
+				{
+					Payload extracted;
+					if(filterItem == null)
+						extracted = handler.extract(new AnyFilter(0), source.dir ^ 1, true);
+					else
+						extracted = handler.extract(filterItem, source.dir ^ 1, filterItem.size(), mSizeMode, true);
+					if(extracted != null)
+					{
+						TubeItem tItem = new TubeItem(extracted);
+						tItem.state = TubeItem.IMPORT;
+						tItem.direction = source.dir ^ 1;
+						
+						PathLocation tubeLoc = new PathLocation(source, source.dir ^ 1);
+						TileEntity tile = CommonHelper.getTileEntity(world(), tubeLoc.position);
+						ITubeConnectable con = TubeHelper.getTubeConnectable(tile);
+						if(con != null)
+							con.addItem(tItem, true);
+						
+						--mPulses;
+						if(mPulses < 0)
+							mPulses = 0;
+						
+						if(mMode == PullMode.RedstoneSingle)
+							openChannel(CHANNEL_PULSE);
+						
+						return;
+					}
+				}
 			}
 		}
+	}
+	
+	@Override
+	public boolean isEndPointOk( Position position, int fromSide )
+	{
+		IPayloadHandler handler = InteractionHandler.getHandler((mActiveFilter == null ? null : mActiveFilter.getPayloadType()), world(), position);
+		if(handler != null)
+		{
+			Payload extracted;
+			if(mActiveFilter == null)
+				extracted = handler.extract(new AnyFilter(0), fromSide ^ 1, false);
+			else
+				extracted = handler.extract(mActiveFilter, fromSide ^ 1, mActiveFilter.size(), mSizeMode, false);
+			
+			if(extracted != null)
+			{
+				TubeItem tItem = new TubeItem(extracted);
+				tItem.state = TubeItem.IMPORT;
+				tItem.direction = fromSide ^ 1;
+				
+				Position routePos = position.copy().offset(fromSide ^ 1, 1);
+				if(routePos.equals(new Position(x(),y(),z())))
+					return true;
+				return (new InputRouter(world(), routePos, tItem).route() != null);
+			}
+		}
+		
+		return false;
 	}
 	
 	@Override
 	public boolean canAcceptOverflowFromSide( int side )
 	{
 		return (side == getFacing());
-	}
-	
-	@Override
-	public OverflowBuffer getOverflowContents()
-	{
-		return mOverflow;
 	}
 	
 	@Override
@@ -163,17 +217,17 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 	public void simulateEffects( TubeItem item )
 	{
 		item.colour = mColor;
-		item.goal = TubesAPI.goalOutput;
+		item.state = TubeItem.NORMAL;
 	}
 	
 	@Override
 	public int onDetermineDestination( TubeItem item )
 	{
-		if(item.goal != TubesAPI.goalInput)
+		if(item.state != TubeItem.IMPORT)
 			return item.direction ^ 1;
 		
 		item.colour = mColor;
-		item.goal = TubesAPI.goalOutput;
+		item.state = TubeItem.NORMAL;
 		
 		return getFacing() ^ 1;
 	}
@@ -181,9 +235,9 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 	@Override
 	public boolean canItemEnter( TubeItem item )
 	{
-		if(item.goal == TubesAPI.goalOverflow && item.direction == getFacing())
+		if(item.state == TubeItem.BLOCKED && item.direction == getFacing())
 			return true;
-		else if(item.goal != TubesAPI.goalInput)
+		else if(item.state != TubeItem.IMPORT)
 			return false;
 		
 		if(item.direction != (getFacing() ^ 1))
@@ -201,9 +255,15 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 			empty = false;
 			
 			if(mFilter[i].matches(item, SizeMode.Max))
-				return true;
+			{
+				if(mIsOpen > 0 || mMode == PullMode.Constant || (mMode == PullMode.RedstoneConstant && mIsPowered))
+					return true;
+				else
+					return false;
+			}
 		}
-		
+		Position position = new Position(x(), y(), z());
+		// MinecraftServer.getServer().getConfigurationManager().sendChatMsg(new ChatComponentText("Returning Empty at "+ position.x + "," + position.y +","+position.z));
 		return empty;
 	}
 	
@@ -224,18 +284,16 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 			if(mFilter[i].matches(payload, SizeMode.Max))
 				return true;
 		}
-		
 		return empty;
 	}
 	
 	@Override
 	protected boolean onItemJunction( TubeItem item )
 	{
-		if(item.goal == TubesAPI.goalOverflow)
+		if(item.state == TubeItem.BLOCKED)
 		{
 			if(!world().isRemote)
 				mOverflow.addItem(item);
-			
 			return false;
 		}
 
@@ -244,17 +302,14 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 			item.lastDirection = item.direction;
 			item.direction = getFacing() ^ 1;
 			item.updated = true;
-			
 			return true;
 		}
 		else if(!mOverflow.isEmpty())
 		{
 			if(!world().isRemote)
 				mOverflow.addItem(item);
-			
 			return false;
 		}
-		
 		return super.onItemJunction(item);
 	}
 	
@@ -284,13 +339,16 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 				openChannel(CHANNEL_POWERED).writeBoolean(state);
 			
 			if(!mIsPowered && state && mMode == PullMode.RedstoneSingle)
+			{
+				++mIsOpen;
 				++mPulses;
+			}
 	
 			mIsPowered = state;
 		}
 		else
 		{
-			switch(mMode)
+			switch(getMode())
 			{
 			case Constant:
 				animTime += 0.05f;
@@ -351,40 +409,40 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 	public void setFilter(int slot, IFilter filter)
 	{
 		mFilter[slot] = filter;
-		tile().markDirty();
-	}
-		
-	@Override
-	public <T> T getProperty( int prop )
-	{
-		switch(prop)
-		{
-		case PROP_MODE:
-			return (T)mMode;
-		case PROP_SIZEMODE:
-			return (T)mSizeMode;
-		case PROP_COLOR:
-			return (T)Integer.valueOf(mColor);
-		}
-		return null;
 	}
 	
-	@Override
-	public <T> void setProperty( int prop, T value )
+	public PullMode getMode()
 	{
-		switch(prop)
-		{
-		case PROP_MODE:
-			mMode = (PullMode)value;
-			break;
-		case PROP_SIZEMODE:
-			mSizeMode = (SizeMode)value;
-			break;
-		case PROP_COLOR:
-			mColor = ((Number)value).intValue();
-			break;
-		}
-		tile().markDirty();
+		return mMode;
+	}
+	
+	public void setMode(PullMode mode)
+	{
+		mMode = mode;
+		if(!world().isRemote)
+			openChannel(CHANNEL_MODE).writeByte(mode.ordinal());
+	}
+	
+	public SizeMode getSizeMode()
+	{
+		return mSizeMode;
+	}
+	
+	public void setSizeMode(SizeMode mode)
+	{
+		mSizeMode = mode;
+		if(!world().isRemote)
+			openChannel(CHANNEL_SIZEMODE).writeByte(mode.ordinal());
+	}
+	
+	public short getColour()
+	{
+		return (short)mColor;
+	}
+	
+	public void setColour(short colour)
+	{
+		mColor = colour;
 	}
 	
 	public boolean isPowered()
@@ -456,6 +514,7 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 		root.setString("PullMode", mMode.name());
 		root.setString("SizeMode", mSizeMode.name());
 		root.setInteger("Pulses", mPulses);
+		root.setInteger("IsOpen", mIsOpen);
 		mOverflow.save(root);
 		
 		root.setShort("Color", (short)mColor);
@@ -499,6 +558,8 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 		mMode = PullMode.valueOf(root.getString("PullMode"));
 		
 		mPulses = root.getInteger("Pulses");
+		mIsOpen = root.getInteger("IsOpen");
+		
 		mOverflow.load(root);
 		
 		if(root.hasKey("Color"))
@@ -520,4 +581,60 @@ public class RequestingTube extends DirectionalTube implements ITubeImportDest, 
 		}
 		return false;
 	}
+	
+	@Override
+	protected boolean onItemLeave(TubeItem item) 
+	{
+		if(item.direction == 6)
+			return false;
+		
+		ForgeDirection dir = ForgeDirection.getOrientation(item.direction);
+		
+		TileEntity ent = world().getTileEntity(x() + dir.offsetX, y() + dir.offsetY, z() + dir.offsetZ);
+		ITubeConnectable con = TubeHelper.getTubeConnectable(ent);
+		if(con != null)
+		{
+			if(world().isRemote && !(con instanceof ITube))
+				return true;
+			
+			if(con.canItemEnter(item))
+			{
+				item.progress -= 1;
+				item.lastProgress -= 1;
+				
+				item.updated = false;
+				
+				
+				boolean exit = con.addItem(item); 
+				if(exit)
+				{
+					--mIsOpen;
+					if(mIsOpen < 0)
+						mIsOpen = 0;
+				}
+				return exit;
+			}
+			else
+				return false;
+		}
+		
+		if(world().isRemote)
+			return true;
+		
+		IPayloadHandler handler = InteractionHandler.getHandler(item.item.getClass(), world(), x() + dir.offsetX, y() + dir.offsetY, z() + dir.offsetZ);
+	
+		if(handler != null)
+		{
+			Payload remaining = handler.insert(item.item, item.direction ^ 1, true);
+			if(remaining == null)
+			{
+				--mIsOpen;
+				return true;
+			}
+			
+			item.item = remaining;
+		}
+		
+		return false;
+	}	
 }
